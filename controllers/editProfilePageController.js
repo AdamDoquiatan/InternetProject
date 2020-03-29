d5e1a8fcbfb3c1088062f5725bd1fbcf66c49a1f
AdamDoquiatan <adamdoq@gmail.com>
2020-03-28 18:36:48 -0700

    profilePage and editProfilePage render initial data

--- controllers/editProfilePageController.js
+++ controllers/editProfilePageController.js
@@ -10,9 +10,8 @@ exports.renderEditProfilePage = async (req, res) => {
 	try {
 		// These functions pull data needed to render the page from the database. Then you can do whatever you want with it.
 		// Right now we're just rendering the raw data to the screen
-		// const userData = await userModel.getProfile({})
-		// res.render('editProfilePage', { user_data: userData })
-		res.render('editProfilePage', { editProfilePageJSCSS: true })
+		const userData = await userModel.getUserProfile({ user_id: req.session.userId })
+		res.render('editProfilePage', { editProfilePageJSCSS: true, user_data: JSON.stringify(userData) })
 	} catch (err) {
 		res.send('' + err)
 	}
--- controllers/profilePageController.js
+++ controllers/profilePageController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const userModel = require('../models/userModel.js')
+const postModel = require('../models/postModel.js')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,9 +11,14 @@ exports.renderProfilePage = async (req, res) => {
 	try {
 		// These functions pull data needed to render the page from the database. Then you can do whatever you want with it.
 		// Right now we're just rendering the raw data to the screen
-		// const userData = await userModel.getProfile({})
-		// res.render('renderProfilePage', { user_data: userData })
-		res.render('ProfilePage', { profilePageJSCSS: true })
+		// NOTE: whatever link calls this needs to add the target userId to the query string
+		const userData = await userModel.getUserProfile({ user_id: req.query.userId })
+		const postData = await postModel.getAllPostsByUser({ user_id: req.query.userId })
+		res.render('profilePage', {
+			profilePageJSCSS: true,
+			user_data: JSON.stringify(userData),
+			post_data: JSON.stringify(postData)
+		})
 	} catch (err) {
 		res.send('' + err)
 	}
