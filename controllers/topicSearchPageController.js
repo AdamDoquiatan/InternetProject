8b8f3502ed764982acb681af42f7d5601cfb147f
AdamDoquiatan <adamdoq@gmail.com>
2020-04-04 17:49:07 -0700

    topicSearchPage renders post partials

--- controllers/topicSearchPageController.js
+++ controllers/topicSearchPageController.js
@@ -8,11 +8,9 @@ app.use(bodyParser.json())
 
 exports.renderTopicSearchPage = async (req, res) => {
 	try {
-		// These functions pull data needed to render the page from the database. Then you can do whatever you want with it.
-		// Right now we're just rendering the raw data to the screen
 		const queryString = req.query.topic
 		const postData = await postModel.getPostsByTopic({ queryString: queryString })
-		res.render('topicSearchPage', { topicSearchPageJSCSS: true, post_data: JSON.stringify(postData) })
+		res.render('topicSearchPage', { topicSearchPageJSCSS: true, post_data: postData })
 	} catch (err) {
 		res.send('' + err)
 	}
