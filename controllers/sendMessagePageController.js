d67a1b6f74d8f46026bf7c4f3dcf2e3da2ae0315
Jason <jasonsbtang@gmail.com>
2020-04-07 13:27:57 -0700

    -createConvo implemented. Sending messages directly now creates a convo."

--- controllers/sendMessagePageController.js
+++ controllers/sendMessagePageController.js
@@ -2,32 +2,46 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const messageModel = require('../models/messageModel')
+const convoModel = require('../models/convoModel')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-exports.renderSendMessagePage = async (req, res) => {
-	try {
-		let createMessageInfo = {
-			convo_id: 69,
-			sender_user_id: req.session.userId,
-			reciever_user_id: req.query.userId,
-			sender_full_name: req.session.fullName,
-			user_img_url: req.session.userImgUrl
-		}
-		res.render('sendMessagePage', { sendMessagePageJSCSS: true, message_params: createMessageInfo })
-	} catch (err) {
-		res.send('' + err)
-	}
+exports.renderSendMessagePage = async(req, res) => {
+    try {
+
+        let createMessageInfo = {
+            sender_user_id: req.session.userId,
+            reciever_user_id: req.query.userId,
+            sender_full_name: req.session.fullName,
+            user_img_url: req.session.userImgUrl
+        }
+        res.render('sendMessagePage', { sendMessagePageJSCSS: true, message_params: createMessageInfo })
+    } catch (err) {
+        res.send('' + err)
+    }
 }
 
-exports.sendMessage = async (req, res) => {
-	try {
-		console.log(JSON.stringify(req.body))
-		await messageModel.createMessage(req.body)
-		await messageModel.sendMessageEmail(req.body)
-		res.redirect('/profilePage?userId=' + req.body.reciever_user_id)
-	} catch (err) {
-		res.send('' + err)
-	}
+exports.sendMessage = async(req, res) => {
+    try {
+        console.log(JSON.stringify(req.body))
+        const convoDetails = {
+            senderId: req.session.userId,
+            recieverId: req.query.userId,
+            imgUrl: req.session.userImgUrl
+        }
+        let convoID = await convoModel.createConvo(convoDetails)
+        let messageData = {
+            convo_id: convoID,
+            sender_user_id: req.body.sender_user_id,
+            reciever_user_id: req.body.reciever_user_id,
+            sender_full_name: req.body.sender_full_name,
+            user_img_url: req.body.user_img_url
+        }
+        await messageModel.createMessage(messageData)
+        await messageModel.sendMessageEmail(messageData)
+        res.redirect('/profilePage?userId=' + req.body.reciever_user_id)
+    } catch (err) {
+        res.send('' + err)
+    }
 }
--- models/convoModel.js
+++ models/convoModel.js
@@ -1,51 +1,59 @@
 const pool = require('../util/database')
 
 //TODO:
-exports.createConvo = async (userData) => {
-	try {
-		const response = await pool.query()
-		if (response[0].length !== 0) {
-			const responseData = response[0]
-			return responseData
-		} else {
-			return {}
-		}
-	} catch (err) {
-		throw err
-	}
+exports.createConvo = async(userData) => {
+    try {
+        const sql = 'INSERT INTO convos(user1_id, user2_id, user2_img_url) VALUES ("' +
+            userData.senderId +
+            '", "' +
+            userData.recieverId +
+            '", "' +
+            userData.imgUrl +
+            '");'
+        const response = await pool.query(sql)
+        if (response[0].length !== 0) {
+            const responseData = response[0]['insertId']
+            console.log(responseData)
+            return responseData
+        } else {
+            return {}
+        }
+    } catch (err) {
+        throw err
+    }
 }
 
-exports.getAllUsersConvos = async (userData) => {
-	try {
-		const response = await pool.query(
-			'SELECT * FROM convos WHERE user1_id = "' +
-				userData.user_id +
-				'" OR user2_id ="' +
-				userData.user_id +
-				'" ORDER BY updated_at DESC;'
-		)
-		if (response[0].length !== 0) {
-			const responseData = response[0]
-			return responseData
-		} else {
-			return {}
-		}
-	} catch (err) {
-		throw err
-	}
+exports.getAllUsersConvos = async(userData) => {
+    try {
+        const response = await pool.query(
+            'SELECT * FROM convos WHERE user1_id = "' +
+            userData.user_id +
+            '" OR user2_id ="' +
+            userData.user_id +
+            '" ORDER BY updated_at DESC;'
+        )
+        if (response[0].length !== 0) {
+            const responseData = response[0]
+            return responseData
+        } else {
+            return {}
+        }
+    } catch (err) {
+        throw err
+    }
 }
 
 // SQL Query Template
-exports.__nameHere__ = async (userData) => {
-	try {
-		const response = await pool.query()
-		if (response[0].length !== 0) {
-			const responseData = response[0]
-			return responseData
-		} else {
-			return {}
-		}
-	} catch (err) {
-		throw err
-	}
+exports.__nameHere__ = async(userData) => {
+    try {
+        const response = await pool.query()
+        if (response[0].length !== 0) {
+            const responseData = response[0]
+            return responseData
+        } else {
+            return {}
+        }
+    } catch (err) {
+        throw err
+    }
 }
